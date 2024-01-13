b1c60f36c4024ad93335f4f59faa8942e753635a
nikita <nikita.borisenckow@yandex.com>
2024-01-14 00:06:33 +0300

    urlshortener POST test added

--- internal/app/handlers/urlShortenerHandler_test.go
+++ internal/app/handlers/urlShortenerHandler_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -16,17 +17,41 @@ func TestURLShortenerGETHandler(t *testing.T) {
 		{
 			name:       "URL doesnt exist",
 			query:      "/veryLongUrlWichShouldntExistIhopeForIt",
-			statusWant: 400,
+			statusWant: http.StatusBadRequest,
 		},
 		{
 			name:       "Bad request",
 			query:      "/",
-			statusWant: 400,
+			statusWant: http.StatusBadRequest,
 		},
 	}
 
 	for _, tt := range tests {
 		req := httptest.NewRequest(http.MethodGet, tt.query, nil)
+		recorder := httptest.NewRecorder()
+		URLShortenerHandler(recorder, req)
+		res := recorder.Result()
+		require.Equal(t, tt.statusWant, res.StatusCode, tt.name)
+	}
+}
+
+func TestURLShortenerPOSTHandler(t *testing.T) {
+	tests := []struct {
+		name       string
+		query      string
+		body       string
+		statusWant int
+	}{
+		{
+			name:       "Normal one",
+			query:      "/",
+			body:       "https://practicum.yandex.ru/",
+			statusWant: http.StatusCreated,
+		},
+	}
+
+	for _, tt := range tests {
+		req := httptest.NewRequest(http.MethodPost, tt.query, strings.NewReader(tt.body))
 		recorder := httptest.NewRecorder()
 		URLShortenerHandler(recorder, req)
 		res := recorder.Result()
