3b1a4cdad3064cbc68b46f339d4965a78f707637
nikita <nikita.borisenckow@yandex.com>
2024-01-16 23:44:58 +0300

    flags logic was moved to a standart library (instead of  using)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"flag"
 	"github.com/Lesnoi3283/url_shortener/config"
 	"github.com/Lesnoi3283/url_shortener/internal/app/handlers"
-	"github.com/spf13/pflag"
 	"log"
 	"net/http"
 )
 
 func init() {
-	pflag.StringVarP(&config.ServerAddress, "address", "a", "localhost:8080", "Address where server will work. Example: \"localhost:8080\".")
-	pflag.StringVarP(&config.DefaultShortAddress, "base_address", "b", "http://localhost:8080/", "Base address before a shorted URL")
+	flag.StringVar(&config.ServerAddress, "a", "localhost:8080", "Address where server will work. Example: \"localhost:8080\".")
+	flag.StringVar(&config.DefaultShortAddress, "b", "http://localhost:8080/", "Base address before a shorted URL")
 }
 
 func main() {
-	pflag.Parse()
+	flag.Parse()
 
 	r := handlers.MyRouter()
 	log.Fatal(http.ListenAndServe(config.ServerAddress, r))
