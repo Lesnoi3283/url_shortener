bef178b8d6fe21153d95559a3656ad1a663338a8
nikita <nikita.borisenckow@yandex.com>
2024-01-16 23:39:12 +0300

    flags support added. -a & -b works now

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"github.com/Lesnoi3283/url_shortener/config"
 	"github.com/Lesnoi3283/url_shortener/internal/app/handlers"
+	"github.com/spf13/pflag"
 	"log"
 	"net/http"
 )
 
+func init() {
+	pflag.StringVarP(&config.ServerAddress, "address", "a", "localhost:8080", "Address where server will work. Example: \"localhost:8080\".")
+	pflag.StringVarP(&config.DefaultShortAddress, "base_address", "b", "http://localhost:8080/", "Base address before a shorted URL")
+}
+
 func main() {
+	pflag.Parse()
+
 	r := handlers.MyRouter()
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", r))
+	log.Fatal(http.ListenAndServe(config.ServerAddress, r))
 }
