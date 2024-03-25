de2c385ea4284327ec8981083f468cce836e50b8
Nikita <>
2024-03-25 15:43:50 +0300

    postgres table creation added

--- pkg/databases/postgresql/postgresql.go
+++ pkg/databases/postgresql/postgresql.go
@@ -13,12 +13,22 @@ func (p *Postgresql) Ping() error {
 	return p.store.Ping()
 }
 
+func (p *Postgresql) Close() error {
+	return p.store.Close()
+}
+
 func NewPostgresql(connStr string) (Postgresql, error) {
 	db, err := sql.Open("pgx", connStr)
 
 	toRet := Postgresql{
 		store: db,
 	}
+
+	if err != nil {
+		return toRet, err
+	}
+
+	_, err = toRet.store.Exec("CREATE TABLE IF NOT EXISTS urls (id SERIAL PRIMARY KEY, long VARCHAR(2048), short VARCHAR(255));")
 
 	return toRet, err
 }
