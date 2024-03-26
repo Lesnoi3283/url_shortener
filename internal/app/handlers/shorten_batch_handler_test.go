f870dc478c5d01e83fe5dee7d3589995c79340ad
Nikita <>
2024-03-26 03:13:17 +0300

    fix 3

--- internal/app/handlers/shorten_batch_handler_test.go
+++ internal/app/handlers/shorten_batch_handler_test.go
@@ -1,9 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/Lesnoi3283/url_shortener/config"
 	"github.com/Lesnoi3283/url_shortener/internal/app/handlers/mocks"
-	"github.com/Lesnoi3283/url_shortener/pkg/databases/justamap"
+	"github.com/Lesnoi3283/url_shortener/pkg/databases/postgresql"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -52,7 +53,8 @@ func TestShortenBatchHandler_ServeHTTP(t *testing.T) {
 		ServerAddress: "localhost:8080",
 		LogLevel:      "info",
 	}
-	URLStore := justamap.NewJustAMap()
+	URLStore, err := postgresql.NewPostgresql("host=localhost user=yaurlshortenet password=123 dbname=urlshortenerdb sslmode=disable")
+	require.NoError(t, err)
 	logLevel, err := zap.ParseAtomicLevel(conf.LogLevel)
 	if err != nil {
 		log.Fatalf("logger was not started, err: %v", err)
@@ -74,19 +76,48 @@ func TestShortenBatchHandler_ServeHTTP(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			req, err := http.NewRequest(tt.method, ts.URL+tt.query, strings.NewReader(tt.reqBody))
-			require.NoError(t, err, "Creating request should not error")
+			require.NoError(t, err, "Error while creating a request")
 
 			resp, err := http.DefaultClient.Do(req)
-			require.NoError(t, err, "Making request should not error")
+			require.NoError(t, err, "Error while making a request")
 
 			defer resp.Body.Close()
-			assert.Equal(t, tt.statusWant, resp.StatusCode, "Status code should match expected")
+			assert.Equal(t, tt.statusWant, resp.StatusCode, "Wrong status code")
 
 			if !tt.wantEmptyBody {
 				body, err := io.ReadAll(resp.Body)
 				require.NoError(t, err, "Reading response body should not error")
 				require.NotEmpty(t, body, "Response body should not be empty")
+
+				type URLShorten struct {
+					CorrelationID string `json:"correlation_id"`
+					ShortURL      string `json:"short_url"`
+				}
+				var URLsToReturn []URLShorten
+				err = json.Unmarshal(body, &URLsToReturn)
+				require.NoError(t, err, "Unmarshalling response error")
+
+				for _, urlShort := range URLsToReturn {
+					splittedURL := strings.Split(string(urlShort.ShortURL), "/")
+					urlToAsk := ts.URL + "/" + splittedURL[len(splittedURL)-1]
+
+					req2, err := http.NewRequest(http.MethodGet, urlToAsk, nil)
+					require.NoError(t, err, "Error while making a request")
+
+					//to catch redirect
+					ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+						return http.ErrUseLastResponse
+					}
+
+					resp2, err := ts.Client().Do(req2)
+					require.NoError(t, err)
+
+					assert.Equal(t, http.StatusTemporaryRedirect, resp2.StatusCode)
+
+					resp2.Body.Close()
+				}
 			}
+
 		})
 	}
 }
